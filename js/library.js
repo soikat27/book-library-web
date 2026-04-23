76931f80cc296e574cbcbb72a6192df37d007134
Soikat <193371447+soikat27@users.noreply.github.com>
2026-04-23 01:18:53 -0400

    Implement feature: checkbox state and original book.readStatus state match
    -- on check/uncheck, associated book's readStatus changes to reflect the UI change

--- js/library.js
+++ js/library.js
@@ -31,7 +31,7 @@ function addBookToLibrary(event)
     const read = document.getElementById("read").checked;
 
     const newBook = new Book(title, author, pages, read);
-    createBookColumn(title, author, pages, newBook.id);
+    createBookColumn(title, author, pages, newBook.id, read);
     myLibrary.push(newBook);
     dialog.close();
 }
@@ -45,21 +45,21 @@ function displayBooks()
 }
 
 
-function createBookColumn(title, author, totalPages, bookId)
+function createBookColumn(title, author, totalPages, bookId, readStatus)
 {
     const html = `
     <div class="book" data-id="${bookId}">
-      <h3 class="title">${title}</h3>
-      <h3 class="author">${author}</h3>
-      <h3 class="pc">${totalPages}</h3>
-      <div>
-        <input type="checkbox" class="rs">
+    <h3 class="title">${title}</h3>
+    <h3 class="author">${author}</h3>
+    <h3 class="pc">${totalPages}</h3>
+    <div>
+        <input type="checkbox" class="rs" ${readStatus ? "checked" : ""}>
         <button class="remove">
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
                 <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M16.414,15 c0,0,3.139,3.139,3.293,3.293c0.391,0.391,0.391,1.024,0,1.414c-0.391,0.391-1.024,0.391-1.414,0C18.139,19.554,15,16.414,15,16.414 s-3.139,3.139-3.293,3.293c-0.391,0.391-1.024,0.391-1.414,0c-0.391-0.391-0.391-1.024,0-1.414C10.446,18.139,13.586,15,13.586,15 s-3.139-3.139-3.293-3.293c-0.391-0.391-0.391-1.024,0-1.414c0.391-0.391,1.024-0.391,1.414,0C11.861,10.446,15,13.586,15,13.586 s3.139-3.139,3.293-3.293c0.391-0.391,1.024-0.391,1.414,0c0.391,0.391,0.391,1.024,0,1.414C19.554,11.861,16.414,15,16.414,15z"></path>
             </svg>
         </button>
-      </div>
+    </div>
     </div>
     <hr>`;
 
@@ -90,6 +90,19 @@ bookshelf.addEventListener("click", function (event) {
         book.remove();
         if (adjHr)
             adjHr.remove();
+    }
+
+    else if (event.target.closest(".rs"))
+    {
+        const bookElement = event.target.closest(".book");
+        if (!bookElement)
+            return;
+        const bookId = bookElement.dataset.id;
+
+        const book = myLibrary.find(book => book.id === bookId);
+        book.readStatus = !book.readStatus;
+
+        console.log(myLibrary);
     }
 });
 
